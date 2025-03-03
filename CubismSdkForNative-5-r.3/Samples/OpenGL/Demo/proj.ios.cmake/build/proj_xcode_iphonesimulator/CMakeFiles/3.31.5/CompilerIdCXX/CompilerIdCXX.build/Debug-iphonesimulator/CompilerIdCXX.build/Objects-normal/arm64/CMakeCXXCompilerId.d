003b20805b6b58a19f04adc671bdc4a2b5db48ba
Niyaoyao <nycode.jn@gmail.com>
2025-03-03 23:29:47 +0800

    feat: live2d 5.0

---
+++ CubismSdkForNative-5-r.3/Samples/OpenGL/Demo/proj.ios.cmake/build/proj_xcode_iphonesimulator/CMakeFiles/3.31.5/CompilerIdCXX/CompilerIdCXX.build/Debug-iphonesimulator/CompilerIdCXX.build/Objects-normal/arm64/CMakeCXXCompilerId.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/niyao/Downloads/CubismSdkForNative-5-r.3/Samples/OpenGL/Demo/proj.ios.cmake/build/proj_xcode_iphonesimulator/CMakeFiles/3.31.5/CompilerIdCXX/CMakeCXXCompilerId.cpp
---
+++ CubismSdkForNative-5-r.3/deploy.sh
@@ -0,0 +1,12 @@
+export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890
+ls
+cd Samples/OpenGL/thirdParty/scripts
+pwd
+ls
+sh setup_ios_cmake
+pwd
+
+cd ../..
+cd Demo/proj.ios.cmake/scripts
+ls
+sh proj_xcode
