feaaecb437675a36d3bbe1f6048096312a2a8859
NY <niyao@zuoyebang.com>
2025-03-04 19:24:11 +0800

    feat: Plan

---
+++ Live2DSDK-master/Example/Live2DSDK/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Live2DSDK
+//
+//  Created by wudijimao on 03/03/2023.
+//  Copyright (c) 2023 wudijimao. All rights reserved.
+//
+
+import UIKit
+import Live2DSDK
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
