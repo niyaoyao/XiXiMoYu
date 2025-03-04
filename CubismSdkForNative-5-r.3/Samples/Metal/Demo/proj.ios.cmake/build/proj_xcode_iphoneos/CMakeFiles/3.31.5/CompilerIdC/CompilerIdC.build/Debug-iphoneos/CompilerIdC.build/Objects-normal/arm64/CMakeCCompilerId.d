1a662b0098c2ef352ea1a4dbfb3610f79f7ecd92
Niyaoyao <nycode.jn@gmail.com>
2025-03-04 10:05:58 +0800

    feat: arm64 for iphone

---
+++ CubismSdkForNative-5-r.3/Samples/Metal/Demo/proj.ios.cmake/build/proj_xcode_iphoneos/CMakeFiles/3.31.5/CompilerIdC/CompilerIdC.build/Debug-iphoneos/CompilerIdC.build/Objects-normal/arm64/CMakeCCompilerId.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/niyao/NY/Swift/Live2DDemo/CubismSdkForNative-5-r.3/Samples/Metal/Demo/proj.ios.cmake/build/proj_xcode_iphoneos/CMakeFiles/3.31.5/CompilerIdC/CMakeCCompilerId.c
